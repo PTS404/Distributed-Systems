93835fc2503a1b8e1eae76162d602f3553dc1527
Phi Va Lo <phiy@itu.dk>
2023-09-20 13:06:25 +0200

    handin-2 first commit
    Co-authored-by: AnnaJ1006 <AnnaJ1006@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Distributed-Systems
 
-### a) What are packages in your implementation? What data structure do you use to transmit data and meta-data?
+### a) What are packets in your implementation? What data structure do you use to transmit data and meta-data?
+The packets aren't specifically defined but are implemented within a buffer (byte slice), that transmits the data between our client and server. The net package offers the net.conn interface, which handles information in the background.  
 
 ### b) Does your implementation use threads or processes? Why is it not realistic to use threads?
+We are using threads to run client and server simultaneously instead of using the nc command to talk to the tcp server. This is because we don't have linux along wanting to provide a simplified way of using/testing the program.
+
+It is not realistic to use threads since the protocol should run across a network instead of locally. 
 
 ### c) In case the network changes the order in which messages are delivered, how would you handle message re-ordering?
+Using sequence numbers. Gives certain numbers to part of a message (requiring the message to be in more than one part), and then put them in the right sequence.
 
 ### d) In case messages can be delayed or lost, how does your implementation handle message loss?
+This could be solves using either acknowledgements, timeouts or re-transmissions.
+Acknowledgements works by waiting until the message has arrived, this can give some errors, since it will keep waiting. This can be solves working with the timeout, where after a certain amount of time, we assume that the message was never sent. 
+When assuming the message was never sent, we can use re-transmissions to send the request/message again, and then going through the process again. 
 
 ### e) Why is the 3-way handshake important?
+To make sure both the client and the server are agreeing. Both parts need to acknowledge the message. When both the client and server has a connection, then the TCP can move on, they need to have established connection.
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+    // Start the server in a goroutine
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        server()
+    }()
+
+    // Start the client in a goroutine
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        client()
+    }()
+
+	//Wait for the program to finalize
+    wg.Wait()
+}
+
+func server() {
+	//Creates the server
+	server, err := net.Listen("tcp", "localhost:8080") //Listens for connections on localhost and port 8080
+	if err != nil {
+		panic(err) //Throws an exception - Prints message and function then crashes - https://www.educative.io/answers/what-is-panic-in-golang
+	}
+	defer server.Close() //Closes the connection once the program terminates/finishes
+
+	// Split the server address into its host and port components
+	host, port, err := net.SplitHostPort(server.Addr().String())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Listening on host: %s, port: %s\n", host, port)
+
+	//Waits for the next connection and returns it to the listener (var server)
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	//Makes a buffer of 1 kb in size
+	buffer := make([]byte, 1024)
+
+	//Reads the connection and saves the message received
+	msg, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Printf("Error trying to read: %#v\n", err)
+		return
+	}
+
+	//Prints out the received message
+	fmt.Printf("Message received: %s\n", string(buffer[:msg]))
+
+	//Writes back to the client that the message was received
+	conn.Write([]byte("Message received.\n"))
+	
+	//Closes the connection
+	conn.Close()
+}
+
+func client() {
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error trying to connect to the server:", err)
+		return
+	}
+	defer conn.Close() //Closes the connection when the program finishes
+
+	// Send data to the server
+	message := "Hello, Server!"
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		fmt.Println("Error sending the data:", err)
+		return
+	}
+
+	// Receive and print the response from the server
+	buffer := make([]byte, 1024)
+	msg, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error receiving the data:", err)
+		return
+	}
+	response := string(buffer[:msg])
+	fmt.Println("Server response:", response)
+}
